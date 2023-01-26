4f648321559ffd14ab2f57a30a79f43128f2a41b
Lucie Chauvet <l.chauvet@beapp.fr>
2023-01-26 16:15:27 +0100

    Add login and register screens

--- Bilan-Carbone/screens/Auth/LoginScreen.tsx
+++ Bilan-Carbone/screens/Auth/LoginScreen.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import React from "react";
 
 export default function LoginScreen() {
   return (
-    <View>
+    <View style={styles.login}>
       <Text>LoginScreen</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  login: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#32C481",
+  },
+});
--- Bilan-Carbone/screens/Auth/RegisterScreen.tsx
+++ Bilan-Carbone/screens/Auth/RegisterScreen.tsx
@@ -1,10 +1,160 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState, createRef } from "react";
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function RegisterScreen() {
+export const RegisterScreen = (props: any) => {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [errortext, setErrortext] = useState("");
+  const [isRegistrationSuccess, setIsRegistrationSuccess] = useState(false);
+
+  const handleSubmitButton = () => {
+    setErrortext("");
+    if (!userName) {
+      alert("Please fill Name");
+      return;
+    }
+    if (!userEmail) {
+      alert("Please fill Email");
+      return;
+    }
+    if (!userPassword) {
+      alert("Please fill Password");
+      return;
+    }
+  };
+  if (isRegistrationSuccess) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#307ecc",
+          justifyContent: "center",
+        }}
+      >
+        <Text style={styles.successTextStyle}>Inscription réussie !</Text>
+        <TouchableOpacity
+          style={styles.buttonStyle}
+          activeOpacity={0.5}
+          onPress={() => props.navigation.navigate("LoginScreen")}
+        >
+          <Text style={styles.buttonTextStyle}>Se connecter</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
   return (
-    <View>
-      <Text>RegisterScreen</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0F683E" }}>
+      <ScrollView
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          justifyContent: "center",
+          alignContent: "center",
+        }}
+      >
+        <View style={{ alignItems: "center" }}></View>
+        <KeyboardAvoidingView enabled>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserName) => setUserName(UserName)}
+              underlineColorAndroid="#f000"
+              placeholder="Nom d'utilisateur"
+              placeholderTextColor="#8b9cb5"
+              autoCapitalize="sentences"
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserEmail) => setUserEmail(UserEmail)}
+              underlineColorAndroid="#f000"
+              placeholder="Email"
+              placeholderTextColor="#8b9cb5"
+              keyboardType="email-address"
+            />
+          </View>
+          <View style={styles.SectionStyle}>
+            <TextInput
+              style={styles.inputStyle}
+              onChangeText={(UserPassword) => setUserPassword(UserPassword)}
+              secureTextEntry={true}
+              underlineColorAndroid="#f000"
+              placeholder="Mot de passe"
+              placeholderTextColor="#8b9cb5"
+            />
+          </View>
+          {errortext != "" ? (
+            <Text style={styles.errorTextStyle}>{errortext}</Text>
+          ) : null}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            activeOpacity={0.5}
+            onPress={handleSubmitButton}
+          >
+            <Text style={styles.buttonTextStyle}>S'inscrire</Text>
+          </TouchableOpacity>
+        </KeyboardAvoidingView>
+      </ScrollView>
+    </SafeAreaView>
   );
-}
+};
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  SectionStyle: {
+    flexDirection: "row",
+    height: 40,
+    marginTop: 20,
+    marginLeft: 35,
+    marginRight: 35,
+    margin: 10,
+  },
+  buttonStyle: {
+    backgroundColor: "#7DE24E",
+    borderWidth: 0,
+    color: "#FFFFFF",
+    borderColor: "#7DE24E",
+    height: 40,
+    alignItems: "center",
+    borderRadius: 30,
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+  inputStyle: {
+    flex: 1,
+    color: "white",
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderWidth: 1,
+    borderRadius: 30,
+    borderColor: "#dadae8",
+  },
+  errorTextStyle: {
+    color: "red",
+    textAlign: "center",
+    fontSize: 14,
+  },
+  successTextStyle: {
+    color: "white",
+    textAlign: "center",
+    fontSize: 18,
+    padding: 30,
+  },
+});
