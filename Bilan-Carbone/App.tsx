fae10d26d25c8b37148dba2b43b33951d120bf0f
Lucie Chauvet <l.chauvet@beapp.fr>
2023-01-26 16:14:35 +0100

    Add landing screen

--- Bilan-Carbone/App.tsx
+++ Bilan-Carbone/App.tsx
@@ -2,6 +2,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SplashScreen from "./screens/Splash/SplashScreen";
 import OnboardingScreen from "./screens/Onboarding/OnboardingScreen";
+import { RegisterScreen } from "./screens/Auth/RegisterScreen";
+import LandingScreen from "./screens/Landing/LandingScreen";
+import LoginScreen from "./screens/Auth/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -19,6 +22,27 @@ export default function App() {
         <Stack.Screen
           name="Onboarding"
           component={OnboardingScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
           options={{
             headerShown: false,
           }}
---
+++ Bilan-Carbone/screens/Landing/LandingScreen.tsx
@@ -0,0 +1,44 @@
+import { View, Text, Button, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LandingScreen({ navigation }: { navigation: any }) {
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#0F683E" }}>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.5}
+        onPress={() => {
+          navigation.navigate("Login");
+        }}
+      >
+        <Text style={styles.buttonTextStyle}>Me connecter</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.5}
+        onPress={() => {
+          navigation.navigate("Onboarding");
+        }}
+      >
+        <Text style={styles.buttonTextStyle}>M'inscrire</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#32C481",
+    textDecoration: "none",
+    color: "#FFFFFF",
+    borderRadius: 10,
+    padding: 10,
+    margin: 10,
+  },
+  buttonTextStyle: {
+    color: "#FFFFFF",
+    paddingVertical: 10,
+    fontSize: 16,
+  },
+});
