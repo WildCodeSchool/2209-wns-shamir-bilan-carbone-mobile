6c887f8d8b8be9179ec5cc18f6a798d4fe744d82
Lucie Chauvet <l.chauvet@beapp.fr>
2023-01-31 11:40:35 +0100

    Add onboarding screen + slides style

--- Bilan-Carbone/screens/Onboarding/OnboardingScreen.tsx
+++ Bilan-Carbone/screens/Onboarding/OnboardingScreen.tsx
@@ -1,10 +1,138 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Button,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import AppIntroSlider from "react-native-app-intro-slider";
+import { useFonts } from "expo-font";
+
+const slides = [
+  {
+    key: "one",
+    title: "Mesurer",
+    text: "Avoir un suivi régulier de son empreinte carbone, c'est possible !",
+    image: require("../../assets/onboarding-slide-one.png"),
+    backgroundColor: "#59b2ab",
+  },
+  {
+    key: "two",
+    title: "Analyser",
+    text: "Nous vous aidons à comprendre l'impact de vos habitudes de consommation.",
+    image: require("../../assets/onboarding-slide-two.png"),
+    backgroundColor: "#febe29",
+  },
+  {
+    key: "three",
+    title: "Agir",
+    text: "Grâce à nos conseils, réduire votre empreinte carbone n'aura jamais été aussi simple !",
+    image: require("../../assets/onboarding-slide-three.png"),
+    backgroundColor: "#22bcb5",
+  },
+];
 
 export default function OnboardingScreen() {
+  const [showRealApp, setShowRealApp] = useState(false);
+  const [fontsLoaded] = useFonts({
+    "Roboto-Condensed": require("../../assets/fonts/RobotoCondensed-Bold.ttf"),
+  });
+
+  const renderItem = ({
+    item,
+  }: {
+    item: {
+      title: string;
+      text: string;
+      image: any;
+    };
+  }) => {
+    return (
+      <View style={styles.slides}>
+        <Text style={styles.title}>{item.title}</Text>
+        <Image style={styles.image} source={item.image} />
+        <Text style={styles.text}>{item.text}</Text>
+      </View>
+    );
+  };
+
+  const onDone = () => {
+    setShowRealApp(true);
+  };
+
   return (
-    <View>
-      <Text>OnboardingScreen</Text>
-    </View>
+    <>
+      {showRealApp ? (
+        <SafeAreaView style={styles.container}>
+          <View style={styles.container}>
+            <Text style={styles.titleStyle}>
+              React Native App Intro Slider using AppIntroSlider
+            </Text>
+            <Text style={styles.paragraphStyle}>
+              This will be your screen when you click Skip from any slide or
+              Done button at last
+            </Text>
+            <Button
+              title="Show Intro Slider again"
+              onPress={() => setShowRealApp(false)}
+            />
+          </View>
+        </SafeAreaView>
+      ) : (
+        <AppIntroSlider
+          data={slides}
+          renderItem={renderItem}
+          onDone={onDone}
+          showSkipButton={true}
+        />
+      )}
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  titleStyle: {
+    fontSize: 22,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold",
+  },
+  paragraphStyle: {
+    textAlign: "center",
+    color: "#34495e",
+    marginBottom: 5,
+  },
+  slides: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "#bcebce",
+  },
+  title: {
+    marginTop: 50,
+    fontSize: 30,
+    color: "#000",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Roboto-Condensed",
+  },
+  text: {
+    marginBottom: 45,
+    color: "#000",
+    textAlign: "center",
+    fontFamily: "Roboto-Condensed",
+    fontSize: 20,
+  },
+  image: {
+    width: 400,
+    height: 400,
+  },
+});
