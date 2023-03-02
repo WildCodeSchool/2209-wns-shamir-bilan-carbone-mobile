c2a983ac67cac8637e2783083be660a7ffc52e84
Lucie Chauvet <l.chauvet@beapp.fr>
2023-03-02 17:11:06 +0100

    wip : set count

---
+++ Bilan-Carbone/assets/constants.ts
@@ -0,0 +1,10 @@
+
+export const COLORS = {
+    LIGHT_BLUE: "#b1ddf1",
+    LIGHT_GREEN: "#9ae19d",
+    VERY_LIGHT_GREEN: "#d6ffb7",
+    VERY_DARK_GREEN: "#181f1c",
+    WHITE: "#ffffff",
+    LIGHT_BROWN: "#8a817c"
+}
+  
