6899b215cd8adf2a77936dbb69f346e1ac342f15
Lucie Chauvet <l.chauvet@beapp.fr>
2023-03-29 15:24:09 +0200

    Add meals icon

---
+++ Bilan-Carbone/components/Meal/MealsIcon.js
@@ -0,0 +1,31 @@
+import React, { useContext } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { CartContext } from '../../context/MealsContext';
+export function CartIcon({navigation}) {
+  const {getItemsCount} = useContext(CartContext);
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text} 
+        onPress={() => {
+          navigation.navigate('Repas');
+        }}
+      >Mes repas ({getItemsCount()})</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 8,
+    backgroundColor: 'orange',
+    height: 40,
+    padding: 8,
+    borderRadius: 32 / 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 12,
+  },
+});
