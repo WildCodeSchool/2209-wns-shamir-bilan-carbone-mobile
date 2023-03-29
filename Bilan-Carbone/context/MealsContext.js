f54e3257d4c490f4d74bb15677a0d0f9bd83d45b
Lucie Chauvet <l.chauvet@beapp.fr>
2023-03-29 15:23:58 +0200

    Add recipes context

---
+++ Bilan-Carbone/context/MealsContext.js
@@ -0,0 +1,44 @@
+import React, {createContext, useState} from 'react';
+import { getRecipe } from '../utils/RecipesService.js';
+export const CartContext = createContext();
+export function CartProvider(props) {
+  const [items, setItems] = useState([]);
+
+  function addItemToCart(id) {
+    const recipe = getRecipe(id);
+    setItems((prevItems) => {
+      const item = prevItems.find((item) => (item.id == id));
+      if(!item) {
+          return [...prevItems, {
+              id,
+              qty: 1,
+              recipe,
+              totalco2: recipe.co2 
+          }];
+      }
+      else { 
+          return prevItems.map((item) => {
+            if(item.id == id) {
+              item.qty++;
+              item.totalco2 += recipe.co2;
+            }
+            return item;
+          });
+      }
+    });
+}
+function getItemsCount() {
+      return items.reduce((sum, item) => (sum + item.qty), 0);
+  }
+
+  function getTotalco2() {
+      return items.reduce((sum, item) => (sum + item.totalco2), 0);
+  }  
+
+  return (
+    <CartContext.Provider 
+      value={{items, setItems, getItemsCount, addItemToCart, getTotalco2}}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
